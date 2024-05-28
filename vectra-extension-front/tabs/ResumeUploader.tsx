30a252ffba3b134b41f1b1c099179a3f6ce17d29
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-05-28 01:30:13 +0000

    refactor: Remove unused state and update file preview UI

--- vectra-extension-front/tabs/ResumeUploader.tsx
+++ vectra-extension-front/tabs/ResumeUploader.tsx
@@ -8,7 +8,6 @@ const ResumeUploader = () => {
   const [dragging, setDragging] = useState(false); 
   const [isloading , setIsLoading] = useState(false) ;
   const [error,setError] = useState(null);
-  const [cvData,setCvData] = useState(null);
   const isMounted = useRef(true)
   const [selectedFile, setSelectedFile] = useState(undefined)
   const [fileUrl , setFileUrl] = useState(undefined)
@@ -35,12 +34,22 @@ const ResumeUploader = () => {
     setDragging(false)
   }
 
+  const handleCancel = ()=>{
+    setSelectedFile(undefined)
+    setFileUrl(undefined)
+    setError(null)
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true)
+    setError(null)
+    
     const formData = new FormData();
     formData.append("CV",title);
     formData.append("pdf",selectedFile);
     console.log(selectedFile);
+    
     try {
       const response = await fetch('http://localhost:3001/upload', { // '/upload' should be the URL of your server endpoint
         method: 'POST',
@@ -54,34 +63,11 @@ const ResumeUploader = () => {
       const data = await response.json();
       console.log(data);
     } catch (error) {
+      setError(error.message)
       console.log(error)
+    } finally{
+      setIsLoading(false)
     }
-    
-    /*if (!selectedFile) return;
-
-    setIsLoading(true);
-
-    try {
-      const formData = new FormData();
-      formData.append("file", selectedFile);
-
-      const response = await fetch(`http://localhost:3001/upload`, {
-        method: "POST",
-        body: formData,
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const data = await response.json();
-      setCvData(data);
-    } catch (err) {
-      setError(err.message);
-    } finally {
-      setIsLoading(false);
-    }
-    */
   };
    
   
@@ -89,15 +75,27 @@ const ResumeUploader = () => {
     <>
     <Header />
     <div
-      className="flex flex-row items-center justify-center mt-44"
+      className="flex flex-row items-center justify-center mt-36"
       onDrop={handleDrop}
       onDragOver={handleDragOver}
       onDragLeave={handleDragLeave}>
       {selectedFile ? (
         <div className="bg-white px-10 py-5 border-md shadow-lg ring-1 ring-inset ring-gray-300">
-          <h2 className="py-2.5 px-20 me-2 mb-2 text-sm font-semibold w-full text-gray-900 border-dashed focus:outline-none bg-white rounded-md text-center border border-gray-200  focus:z-10 focus:ring-4   dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-            File Preview
-          </h2>
+          {error ? (
+            <div className="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+              <svg className="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+              </svg>
+              <span className="sr-only">Info</span>
+              <div>
+                <span className="font-medium">Server error! </span>Please try again later.
+              </div>
+            </div>
+            ) :(
+              <h2 className="py-2.5 px-20 me-2 mb-2 text-sm font-semibold w-full text-gray-900 border-dashed focus:outline-none bg-white rounded-md text-center border border-gray-200  focus:z-10 focus:ring-4   dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                File Preview
+              </h2>
+            )}
           <embed
             src={fileUrl}
             width="500"
@@ -111,12 +109,19 @@ const ResumeUploader = () => {
               className="rounded-md bg-indigo-500 px-2.5 py-1.5 text-sm  font-semibold text-white hover:shadow-md  ring-inset hover:bg-black-800  w-1/2"
               onClick={handleSubmit}
               >
-              Sumbit
+                {isloading ? (
+                  <svg aria-hidden="true" className="inline w-3 h-5 text-white animate-spin dark:text-white fill-gray-600 dark:fill-gray-300" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                  </svg>
+                ) : (
+                  'Upload'
+                )}
             </button>
             <button
               type="button"
               className="text-sm font-semibold leading-6 text-indigo-300 w-1/2 ring-inset  hover:shadow-sm"
-              onClick={() => setSelectedFile(undefined)}>
+              onClick={handleCancel}>
               Cancel
             </button>
           </div>
